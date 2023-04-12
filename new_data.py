093312296e0d7ca99004ed033ee66e1aada38ebf
Sahijdeep Aujla <117302675+Sahijdeep@users.noreply.github.com>
2023-04-12 15:12:39 -0600

    Update new_data.py

--- new_data.py
+++ new_data.py
@@ -26,23 +26,19 @@ st.write(''' # Academic Success Prediction App''')
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-  Nationality = st.sidebar.slider('Nationality', 1,21,10)
-  Displaced  = st.sidebar.slider('Displaced', 0, 1)
   Tuition_fees_up_to_date= st.sidebar.slider('Tuition fees up to date', 0, 1)
   Scholarship_holder = st.sidebar.slider('Scholarship holder', 0, 1)
-  Curricular_units_1st_sem_approved=st.sidebar.slider('Curricular units 1st sem (approved)', 0,26,13)
-  Curricular_units_1st_sem_grade=st.sidebar.slider('Curricular units 1st sem (grade)', 0,20,10)
-  Curricular_units_2nd_sem_approved=st.sidebar.slider('Curricular units 2nd sem (approved)', 0,26,13)
-  Curricular_units_2nd_sem_grade=st.sidebar.slider('Curricular units 2nd sem (grade)', 0,20,10)
+  Marital_Status = st.sidebar.slider('Marital_Status', 1,4,2)
+  Gender=st.sidebar.slider('Gender', 0,1,0.5)
+  Age at enrollment=st.sidebar.slider('Age at enrollment', 18,55,36)
+  International=st.sidebar.slider('International', 0,1,0.5)
 
-  user_input_data = {'Nationality': Nationality,
-                     'Displaced': Displaced,
-                     'Tuition fees up to date': Tuition_fees_up_to_date,
+  user_input_data = {'Tuition fees up to date': Tuition_fees_up_to_date,
                      'Scholarship holder': Scholarship_holder,
-                     'Curricular units 1st sem (approved)':Curricular_units_1st_sem_approved,
-                     'Curricular units 1st sem (grade)':Curricular_units_1st_sem_grade,
-                     'Curricular units 2nd sem (approved)':Curricular_units_2nd_sem_approved,
-                     'Curricular units 2nd sem (grade)':Curricular_units_2nd_sem_grade}
+                     'Marital Status':Marital_Status,
+                     'Gender':Gender,
+                     'Age':Age at enrollment,
+                     'International':International}
 
   features = pd.DataFrame(user_input_data, index=[0])
 
@@ -56,9 +52,9 @@ st.write(df)
 student = pd.read_csv('dataset.csv')
 st.subheader('Entire Dataset')
 st.write(student)
-student.shape
-student.columns
-student.sample(4)
+#student.shape
+#student.columns
+#student.sample(4)
 #print(student.isnull().sum())
 #print(student.duplicated().sum())
 student['Target'].unique()
@@ -70,7 +66,7 @@ student['Target'] = student['Target'].map({
 #student.describe()
 #student.corr()['Target']
 
-student_df=student.drop(columns=['Marital status','Application mode','Application order','Course','Daytime/evening attendance','Previous qualification',"Mother's qualification","Father's qualification","Mother's occupation","Father's occupation",'Educational special needs','Debtor','Age at enrollment','International','Curricular units 1st sem (credited)','Curricular units 1st sem (enrolled)','Curricular units 1st sem (evaluations)','Curricular units 1st sem (without evaluations)','Curricular units 2nd sem (credited)','Curricular units 2nd sem (enrolled)','Curricular units 2nd sem (evaluations)','Curricular units 2nd sem (without evaluations)','Unemployment rate','Inflation rate','GDP','Target'],axis=1)
+student_df=student.drop(columns=['Nacionality','Displaced','Curricular units 1st sem (approved)','Curricular units 2st sem (approved)','Curricular units 1st sem (grade)','Curricular units 2st sem (grade)','Application mode','Application order','Course','Daytime/evening attendance','Previous qualification',"Mother's qualification","Father's qualification","Mother's occupation","Father's occupation",'Educational special needs','Debtor','Curricular units 1st sem (credited)','Curricular units 1st sem (enrolled)','Curricular units 1st sem (evaluations)','Curricular units 1st sem (without evaluations)','Curricular units 2nd sem (credited)','Curricular units 2nd sem (enrolled)','Curricular units 2nd sem (evaluations)','Curricular units 2nd sem (without evaluations)','Unemployment rate','Inflation rate','GDP','Target'],axis=1)
 
 student_df = student.iloc[:,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]]
 x = student_df['Target'].value_counts().index
@@ -122,6 +118,8 @@ prediction_probabilities = clf.predict_proba(X_test)
 
 #st.subheader('Class labels and their corresponding index number')
 #st.write(y)
+
+print('0 = dropout, 1 = Enrolled, 2 = Graduate')
 
 st.subheader('Prediction Probability')
 st.write(prediction_probabilities)
